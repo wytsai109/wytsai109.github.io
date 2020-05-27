cb09a1cb625ba9e7de9e6312e814f33e67794f8a
wytsai109 <47453915+wytsai109@users.noreply.github.com>
2020-05-27 12:11:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,231 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>M-Dev Store</title>
-    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
-    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="styles/style.css">
+<head>  
+  <title>Hi! I'm Will</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font: 20px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+    background-color: #1abc9c; /* Green */
+    color: #ffffff;
+  }
+  .bg-2 { 
+    background-color: #474e5d; /* Dark Blue */
+    color: #ffffff;
+  }
+  .bg-3 { 
+    background-color: #ffffff; /* White */
+    color: #555555;
+  }
+  .bg-4 { 
+    background-color: #2f2f2f; /* Black Gray */
+    color: #fff;
+  }
+  .container-fluid {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+  .navbar {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: 0;
+    border-radius: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+    color: #1abc9c !important;
+  }
+  </style>
 </head>
 <body>
-   
-   <div id="top"><!-- Top Begin -->
-       
-       <div class="container"><!-- container Begin -->
-           
-           <div class="col-md-6 offer"><!-- col-md-6 offer Begin -->
-               
-               <a href="#" class="btn btn-success btn-sm">Welcome</a>
-               <a href="checkout.php">4 Items In Your Cart | Total Price: $300 </a>
-               
-           </div><!-- col-md-6 offer Finish -->
-           
-           <div class="col-md-6"><!-- col-md-6 Begin -->
-               
-               <ul class="menu"><!-- cmenu Begin -->
-                   
-                   <li>
-                       <a href="customer_register.php">Register</a>
-                   </li>
-                   <li>
-                       <a href="checkout.php">My Account</a>
-                   </li>
-                   <li>
-                       <a href="cart.php">Go To Cart</a>
-                   </li>
-                   <li>
-                       <a href="checkout.php">Login</a>
-                   </li>
-                   
-               </ul><!-- menu Finish -->
-               
-           </div><!-- col-md-6 Finish -->
-           
-       </div><!-- container Finish -->
-       
-   </div><!-- Top Finish -->
-   
-   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default Begin -->
-       
-       <div class="container"><!-- container Begin -->
-           
-           <div class="navbar-header"><!-- navbar-header Begin -->
-               
-               <a href="index.php" class="navbar-brand home"><!-- navbar-brand home Begin -->
-                   
-                   <img src="images/ecom-store-logo.png" alt="M-dev-Store Logo" class="hidden-xs">
-                   <img src="images/ecom-store-logo-mobile.png" alt="M-dev-Store Logo Mobile" class="visible-xs">
-                   
-               </a><!-- navbar-brand home Finish -->
-               
-               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
-                   
-                   <span class="sr-only">Toggle Navigation</span>
-                   
-                   <i class="fa fa-align-justify"></i>
-                   
-               </button>
-               
-               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
-                   
-                   <span class="sr-only">Toggle Search</span>
-                   
-                   <i class="fa fa-search"></i>
-                   
-               </button>
-               
-           </div><!-- navbar-header Finish -->
-           
-           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse Begin -->
-               
-               <div class="padding-nav"><!-- padding-nav Begin -->
-                   
-                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
-                       
-                       <li class="active">
-                           <a href="index.php">Home</a>
-                       </li>
-                       <li>
-                           <a href="shop.php">Shop</a>
-                       </li>
-                       <li>
-                           <a href="checkout.php">My Account</a>
-                       </li>
-                       <li>
-                           <a href="cart.php">Shopping Cart</a>
-                       </li>
-                       <li>
-                           <a href="contact.php">Contact Us</a>
-                       </li>
-                       
-                   </ul><!-- nav navbar-nav left Finish -->
-                   
-               </div><!-- padding-nav Finish -->
-               
-               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
-                   
-                   <i class="fa fa-shopping-cart"></i>
-                   
-                   <span>4 Items In Your Cart</span>
-                   
-               </a><!-- btn navbar-btn btn-primary Finish -->
-               
-               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Begin -->
-                   
-                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn Begin -->
-                       
-                       <span class="sr-only">Toggle Search</span>
-                       
-                       <i class="fa fa-search"></i>
-                       
-                   </button><!-- btn btn-primary navbar-btn Finish -->
-                   
-               </div><!-- navbar-collapse collapse right Finish -->
-               
-               <div class="collapse clearfix" id="search"><!-- collapse clearfix Begin -->
-                   
-                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form Begin -->
-                       
-                       <div class="input-group"><!-- input-group Begin -->
-                           
-                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
-                           
-                           <span class="input-group-btn"><!-- input-group-btn Begin -->
-                           
-                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary Begin -->
-                               
-                               <i class="fa fa-search"></i>
-                               
-                           </button><!-- btn btn-primary Finish -->
-                           
-                           </span><!-- input-group-btn Finish -->
-                           
-                       </div><!-- input-group Finish -->
-                       
-                   </form><!-- navbar-form Finish -->
-                   
-               </div><!-- collapse clearfix Finish -->
-               
-           </div><!-- navbar-collapse collapse Finish -->
-           
-       </div><!-- container Finish -->
-       
-   </div><!-- navbar navbar-default Finish -->
-   
-   <div class="container" id="slider"><!-- container Begin -->
-       
-       <div class="col-md-12"><!-- col-md-12 Begin -->
-           
-           <div class="carousel slide" id="myCarousel" data-ride="carousel"><!-- carousel slide Begin -->
-               
-               <ol class="carousel-indicators"><!-- carousel-indicators Begin -->
-                   
-                   <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
-                   <li data-target="#myCarousel" data-slide-to="1"></li>
-                   <li data-target="#myCarousel" data-slide-to="2"></li>
-                   <li data-target="#myCarousel" data-slide-to="3"></li>
-                   <li data-target="#myCarousel" data-slide-to="4"></li>
-                   
-               </ol><!-- carousel-indicators Finish -->
-               
-               <div class="carousel-inner"><!-- carousel-inner Begin -->
-                   
-                   <div class="item active">
-                       
-                       <img src="admin_area/slides_images/slide-5.jpg" alt="Slider Image 1">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-6.jpg" alt="Slider Image 2">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-7.jpg" alt="Slider Image 3">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-8.jpg" alt="Slider Image 4">
-                       
-                   </div>
-                   
-                   <div class="item">
-                       
-                       <img src="admin_area/slides_images/slide-5.jpg" alt="Slider Image 5">
-                       
-                   </div>
-               </div><!-- carousel-inner Finish -->
-               
-               <a href="#myCarousel" class="left carousel-control" data-slide="prev"><!-- left carousel-control Begin -->
-                   
-                   <span class="glyphicon glyphicon-chevron-left"></span>
-                   <span class="sr-only">Previous</span>
-                   
-               </a><!-- left carousel-control Finish -->
-               
-               <a href="#myCarousel" class="right carousel-control" data-slide="next"><!-- left carousel-control Begin -->
-                   
-                   <span class="glyphicon glyphicon-chevron-right"></span>
-                   <span class="sr-only">Next</span>
-                   
-               </a><!-- left carousel-control Finish -->
-               
-           </div><!-- carousel slide Finish -->
-           
-       </div><!-- col-md-12 Finish -->
-       
-   </div><!-- container Finish -->
-   
-    
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
-    
-    
+
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Wei-Yang Tsai</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Education</a></li>
+        <li><a href="#">Work Experience</a></li>
+        <li><a href="#">SKILLS, Public repository</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+  <h3 class="margin">National Cheng Kung University Tainan, Taiwan Master of Engineering in Biomedical 2016</h3>
+  <!-- <img src="bird.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350"> -->
+  <h3>GPA: 4.0 / 4.3</h3>
+</div>
+
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center">
+  <h3 class="margin">Work Experience</h3>
+  <h3>Software Engineer, R&D 2017 – 2020</h3>
+  <a href="#" class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-search"></span> Search
+  </a>
+</div>
+
+<!-- Third Container (Grid) -->
+<div class="container-fluid bg-3 text-center">    
+  <h3 class="margin">Where To Find Me?</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds2.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+  <p>Contact<a href="https://www.w3schools.com">wytsai109@gmail.com</a></p> 
+</footer>
+
 </body>
 </html>
